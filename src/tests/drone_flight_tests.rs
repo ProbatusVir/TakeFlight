f24fe9a7dab579bae4b1acdb153a58e505786428
Joshua <joshbear367763@gmail.com>
2025-09-19 15:09:47 -0400

    Created tests to test drone classes, implemented tests for TelloDrone

---
+++ src/tests/drone_flight_tests.rs
@@ -0,0 +1,40 @@
+use rstest::rstest;
+use crate::Error;
+
+mod tello_drone
+{
+	use std::f32::consts::PI;
+	use std::thread::sleep;
+	use std::time::Duration;
+	use super::rstest;
+	use super::Error;
+	use crate::drone_interface::Drone;
+	use crate::drone_interface::tello::drone::Drone as TelloDrone;
+	#[rstest]
+	fn drone_init_test() -> Result<(), Error>
+	{
+		TelloDrone::init()?;
+
+		Ok(())
+	}
+
+	fn simple_planned_flight_test() -> Result<(), Error>
+	{
+		let mut drone = TelloDrone::init()?;
+		sleep(Duration::from_secs(1));
+		drone.takeoff()?;
+
+		// Box flight, each section is 25 centimeters
+		drone.forward(25)?;
+		drone.left(25)?;
+		drone.backward(25)?;
+		drone.right(25)?;
+
+		drone.clockwise_rot(6.0 * PI)?;	// Three rotations clockwise
+		drone.cclockwise_rot(6.0 * PI)?;	// Three rotations counterclockwise
+
+		drone.graceful_land()?;
+
+		Ok(())
+	}
+}
