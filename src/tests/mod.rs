3a2154084ca1857b5c18f2e4af187eed9b382809
Joshua <joshbear367763@gmail.com>
2025-10-05 00:02:53 -0400

    Added a function to the test module

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -6,3 +6,12 @@ mod drone_flight_tests;
 mod computer_vision_test;
 
 const TEST_PATH : &str = "test_results/";
+
+/// width is measured in pixels, and stride is the size of each pixel in bytes.
+fn get_mut_pixel<T>(image : &mut [T], x : usize, y : usize, width : usize, stride : usize) -> &mut [T]
+{
+	let row_first_pixel = y * width;
+	let index = (row_first_pixel + x) * stride;
+	&mut image[index..index + stride]
+}
+
