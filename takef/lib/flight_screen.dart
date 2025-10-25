8a41a0d52bf0b64cb8acd23bc9a021410cc7d502
Rashid Byrdsell <RHByrdsell@outlook.com>
2025-10-25 17:25:22 -0400

    creation of flight screen

---
+++ takef/lib/flight_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class FlightScreen extends StatelessWidget{
+  const FlightScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement flight screen
+    throw UnimplementedError();
+  }
+
+}
