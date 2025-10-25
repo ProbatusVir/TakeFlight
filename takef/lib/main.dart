f3709f86096eb1e70da55f885da7a9b41377ccbd
Rashid Byrdsell <RHByrdsell@outlook.com>
2025-10-25 16:38:26 -0400

    Connect Screen and adding svg dependency

--- takef/lib/main.dart
+++ takef/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; //svg package handler
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +12,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TakeFlight',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.grey.shade700,
+        ),
+        //Color for text theme
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            //color: Colors.white,
+            foreground: Paint()
+              ..style = PaintingStyle.stroke //set the style to stroke
+              ..strokeWidth = 2 //defines the width of the strok
+              ..color = Colors.white, //set the stroke color
+          ),
+          headlineMedium: TextStyle(color: Colors.black), //raw hex value til style file is created
+        ),
+        scaffoldBackgroundColor: Colors.black45,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false, //gets rid of debug sash
+      home: const MyHomePage(title: 'TakeFlight'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,69 +45,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      //TODO::Fix screen background color and change out logo text with svg text
+      backgroundColor: Colors.black45,//changes the overall scaffold color which is the background of the screen itself
+      // settings button
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black45,
+          onPressed: (){},
+          child: Icon(
+            Icons.settings_outlined,
+            color: Colors.white,
+          ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop, //places the setting button to the top right
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
+          //returns multiple child widgets to place in the center
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            //Logo image
+            Image.asset('../Images/drone_icon.png'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'TAKEFLIGHT',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            FloatingActionButton.extended( //extends the button to fit its contents
+              backgroundColor: Colors.grey.shade400, //grey with a shade value of 400 that gives the creamy look
+                onPressed: (){},
+                label: Text(
+                    'CONNECT...',
+                    style: Theme.of(context).textTheme.headlineMedium, //default text size and theme
+                ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
