f3b3b902863ac986e87d34bed6d45e807b933dc0
Rashid Byrdsell <RHByrdsell@outlook.com>
2025-10-29 13:56:44 -0400

    Connecting to server and receiving data untested

--- takef/lib/main.dart
+++ takef/lib/main.dart
@@ -1,9 +1,61 @@
+import 'dart:io';
+import 'dart:convert'; //For encoding/decoding
+import 'dart:typed_data'; //For Uint8List
 import 'package:flutter/material.dart';
 //import 'package:flutter_svg/flutter_svg.dart'; //svg package handler
 import 'flight_screen.dart';
 
 void main() {
   runApp(const MyApp());
+  //Connect to rust server
+  Future<void> connectToServer() async{
+    Socket? socket;
+    try{
+      socket = await Socket.connect('127.0.0.1', 4000);
+      //Prints are for debugging
+      print('Connected to erver: ${socket.remoteAddress}:${socket.remotePort}');
+    }on SocketException catch (e){
+      print("Error connecting to server: $e");
+    }
+    //send data to server
+    if(socket != null){
+      socket.add([0x42, 0x42, 2]); //sends the header bytes along with the ID of video stream
+      socket.flush(); //ensures all data is sent
+    }
+    List<int> imageDataBytes = [];
+    int? imageLength;
+    //receiving image data
+    if(socket != null){
+      socket.listen(
+          (Uint8List data){
+            if(imageLength == null && data.length >= 4){ //Assuming 4 bytes for length
+              imageLength = ByteData.view(data.buffer).getInt32(0, Endian.big); //Read length
+              imageDataBytes.addAll(data.sublist(4)); //read the rest of data after first 4
+            } else if(imageLength != null){
+              imageDataBytes.addAll(data);
+            }
+            if(imageLength != null && imageDataBytes.length >= imageLength!){
+              //image data fully received
+              Uint8List receivedImage = Uint8List.fromList(imageDataBytes.sublist(0, imageLength!));
+              print('Image received with ${receivedImage.length} bytes.');
+
+              // Can now use this function (e.g., display it using Image.memory)
+              // Reset for next image if multiple images are expected
+              imageDataBytes.clear();
+              imageLength = null;
+            }
+          },
+          onDone: (){
+            print('Server disconnected');
+            socket?.destroy();
+          },
+          onError: (error){
+            print('Error on socket: $error');
+            socket?.destroy();
+          }
+      );
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
