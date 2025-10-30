1d9869dd0a3cef1001924fc0e4bc55ceaf861b80
Rashid Byrdsell <RHByrdsell@outlook.com>
2025-10-30 16:21:02 -0400

    Forces screen to landscape for android

--- takef/lib/flight_screen.dart
+++ takef/lib/flight_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'record_button.dart';
+import 'package:flutter/services.dart';
 import 'joy_stick.dart';
 
-class FlightScreen extends StatelessWidget{
+class FlightScreen extends StatefulWidget{
   const FlightScreen({super.key});
+
+  @override
+  State<FlightScreen> createState() => _FlightScreenState();
+}
+class _FlightScreenState extends State<FlightScreen>{
 
   void rc(double lr, double ud, double fb, double rot){
     //TODO::Change to future async once server connection is there
     //simulate movement to drone til connection to server is established
     print('RC Commands: left/right$lr:up/down$ud:forward/backward$fb:rotation$rot'); //debug
+  }
+  //Changed to stateful widget to force this screen into landscape for android
+  @override
+  void initState(){
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+  @override
+  void dispose(){
+    super.dispose();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
   }
   @override
   Widget build(BuildContext context) {
@@ -31,44 +56,44 @@ class FlightScreen extends StatelessWidget{
           Align( //Aligns user menu to bottom center of the screen
             alignment: Alignment.bottomCenter,
             child: Padding(
-                padding: const EdgeInsets.only(bottom: 30), //spacing needed so it isn't touching bottom of the screen
-                child: Container( //The actual circular box
-                  decoration: BoxDecoration(
+              padding: const EdgeInsets.only(bottom: 30), //spacing needed so it isn't touching bottom of the screen
+              child: Container( //The actual circular box
+                decoration: BoxDecoration(
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(50)
-                  ),
-                  child: Row( //Organization of Icons in the box
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly, //Spaces widgets evenly within the Row
-                    mainAxisSize: MainAxisSize.min, //Minimal size needed to fit
-                    children: [
-                      IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.flight_takeoff, color: Colors.white, size: 50.0,)
-                      ),
-                      //TODO::Implement actual recording logic here
-                      RecordButton(),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.settings_outlined, color: Colors.white, size: 50.0,)
-                      ),
-                    ],
-                  ),
                 ),
+                child: Row( //Organization of Icons in the box
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, //Spaces widgets evenly within the Row
+                  mainAxisSize: MainAxisSize.min, //Minimal size needed to fit
+                  children: [
+                    IconButton(
+                        onPressed: (){},
+                        icon: Icon(Icons.flight_takeoff, color: Colors.white, size: 50.0,)
+                    ),
+                    //TODO::Implement actual recording logic here
+                    RecordButton(),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.settings_outlined, color: Colors.white, size: 50.0,)
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-          //TODO::Fix joystick UI for android
+          //TODO::Fix joystick UI and handling for android
           Align( //Joy sticks bottom left
             alignment: Alignment.bottomLeft,
             child: Padding(
-                padding: const EdgeInsets.all(150),
-                child: ThumbStickController(
-                  onChange: (x, y){
-                    //Will be movement logic here
-                    final lr = x;
-                    final fb = y;
-                    rc(x,0,y,0);
-                  },
-                ),
+              padding: const EdgeInsets.all(150),
+              child: ThumbStickController(
+                onChange: (x, y){
+                  //Will be movement logic here
+                  final lr = x;
+                  final fb = y;
+                  rc(x,0,y,0);
+                },
+              ),
             ),
           ),
           Align( //Joy sticks bottom right
@@ -90,5 +115,4 @@ class FlightScreen extends StatelessWidget{
       ),
     );
   }
-
 }
