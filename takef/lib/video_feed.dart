d82ad0cede69c27d1b36b92fb0805c66f82afab2
Rashid Byrdsell <RHByrdsell@outlook.com>
2025-11-09 14:32:22 -0500

    got rid of epilepsy during image to video

--- takef/lib/video_feed.dart
+++ takef/lib/video_feed.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class VideoFeed extends StatefulWidget{
   const VideoFeed({super.key});
@@ -11,12 +12,30 @@ class VideoFeed extends StatefulWidget{
 class _VideoFeedState extends State<VideoFeed>{
   late Timer timer;
   int frame = 0;
-  late List<String> feed;
+  late List<Image> feed;
+  bool isLoad = false;
 
   //on creation of the state sets the list time and frame
   @override void initState() {
     super.initState();
-    feed = List.generate(200, (step) => 'assets/simulated_feed/ezgif-frame-${(step + 1).toString().padLeft(3, '0')}.jpg');
+    loadFeed().then((_){
+      isLoad = true;
+      //setState(() {});
+      start();
+    });
+  }
+  //separate them into functions
+  Future<void> loadFeed() async{
+    feed = [];
+    //loop through feed
+    for(var i = 1; i <= 200; i++){
+      //wait to load all images into memory
+      final bytes = await rootBundle.load('assets/simulated_feed/ezgif-frame-${i.toString().padLeft(3, '0')}.jpg');
+      feed.add(Image.memory(bytes.buffer.asUint8List(), gaplessPlayback: true,));
+    }
+  }
+
+  void start() {
     timer = Timer.periodic(const Duration(milliseconds: 35), (_) {
       setState(() {
         frame = (frame + 1) % feed.length;
@@ -32,6 +51,8 @@ class _VideoFeedState extends State<VideoFeed>{
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(feed[frame], fit: BoxFit.fill); //This will turn into a Image.memory
+    //will place a loading screen if false otherwise will show video feed
+    return isLoad == false ? CircularProgressIndicator()
+      : feed[frame];
   }
 }
