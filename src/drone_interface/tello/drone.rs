d40b56029122243dd6ee5213e96942030d27bd96
Joshua <joshbear367763@gmail.com>
2025-11-11 11:19:07 -0500

    Implemented the setup, connection, takeoff and land. Made some unit tests to compare known packets with attempted packets.

--- src/drone_interface/tello/drone.rs
+++ src/drone_interface/tello/drone.rs
@@ -1,37 +1,47 @@
-use crate::drone_interface::{IUnit, Unit};
+use crate::drone_interface::{Drone, IUnit, Unit};
 use crate::error::Error;
-use crate::{drone_interface, Connection};
-use mio::{Poll, Token};
+use crate::{drone_interface, Connection, Poll, Token, UdpSocket};
 use std::collections::HashMap;
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
-use crate::drone_interface::tello::packet::set_sticks;
+use crate::drone_interface::tello::packet::{land, set_sticks};
+use mio::Interest;
+use zerocopy::IntoBytes;
+use crate::logger::Logger;
+use super::packet;
 
 #[allow(dead_code)]
 #[derive(Debug)]
-pub struct Drone
+pub struct TelloDrone
 {
 	command_sock	: UdpSocket,
 	video_sock		: UdpSocket,
 	info_sock		: UdpSocket,
 	seq_number		: u16,
 	response_buffer	: Vec<u8>,
+	secret			: u16,		// This isn't actually encrypted, but its the value that the drone uses. Just for semantic use.
 
 	forward_percent	: i16,
 	sideway_percent	: i16,
 	rotate_percent	: i16,
 	updown_percent	: i16,
+
+	inner_read_buf	: [u8;4096],
+	logger			: Logger
 }
 
-impl drone_interface::Drone for Drone
+impl drone_interface::Drone for TelloDrone
 {
 	fn takeoff(&mut self) -> Result<(), Error> {
-		self.command_sock.send(b"takeoff")?;
+		/*self.command_sock.send(b"takeoff")?;
 		self.command_sock.recv(&mut self.response_buffer)?;
 
 		sleep(Duration::from_secs(3));
+*/
+		self.command_sock.send(&packet::takeoff(self.seq_number))?;
+		self.seq_number += 1;
 
 		Ok(())
 	}
@@ -46,11 +56,15 @@ impl drone_interface::Drone for Drone
 	}
 
 	fn graceful_land(&mut self) -> Result<(), Error> {
-		self.command_sock.send(b"land")?;
+		/*self.command_sock.send(b"land")?;
 		self.command_sock.recv(&mut self.response_buffer)?;
 
 		sleep(Duration::from_secs(3));
 
+		Ok(())*/
+
+		self.command_sock.send(&land(self.seq_number))?;
+		self.seq_number += 1;
 		Ok(())
 	}
 
@@ -161,59 +175,150 @@ impl drone_interface::Drone for Drone
 	}
 
 	fn receive_signal(&mut self, port: u16) -> Result<(), Error> {
-		dbg!(port);
-		todo!()
+		if port == self.command_sock.local_addr()?.port()
+		{
+			loop {
+				let bytes_read = self.command_sock.recv(&mut self.inner_read_buf)?;
+
+				// Nab the last two bytes. In most messages this is the CRC or the secret.
+				let packet_end = [self.inner_read_buf[bytes_read-2], self.inner_read_buf[bytes_read-1]];
+
+				// This means that it's not formatted like the usual ones, and is probably plain text.
+				if self.inner_read_buf[0] != 0xCC
+				{
+					let message = String::from_utf8_lossy(&self.inner_read_buf[..bytes_read]);
+					self.logger.info_from_string(format!("[[Command Sock Message]]: {}" , crate::debug_utils::raw_hex_to_string(&self.inner_read_buf[..bytes_read])))?;
+
+					if message.contains("conn_ack:")
+					{
+						if self.secret.as_bytes() != packet_end { Err(Error::Custom("Received the wrong value as acknowledgment from Tello."))? }
+						else { self.logger.info("Received handshake acknowledgement from Tello!")? }
+					}
+					else if message.contains("unknown command: ")
+					{
+						self.logger.warn("We provided an unknown command to Tello.")?;
+					}
+					else {
+						println!("no clue what's going on...");
+						todo!("Non 0xCC packets from the Tello that weren't acknowledgement. The message: \"{}\"\t{}", message, message.len()) }
+
+					// TODO: put something here.
+				}
+			}
+		}
+		else if port == self.video_sock.local_addr()?.port()
+		{
+			loop {
+				let bytes_read = self.video_sock.recv(&mut self.inner_read_buf)?;
+				self.logger.info_from_string(format!("[[Video Sock Message]]: {}" , crate::debug_utils::raw_hex_to_string(&self.inner_read_buf[..bytes_read])))?;
+			}
+		}
+		else if port == self.info_sock.local_addr()?.port()
+		{
+			loop {
+				let bytes_read = self.info_sock.recv(&mut self.inner_read_buf)?;
+				self.logger.info_from_string(format!("[[Info Sock Message]]{}" , crate::debug_utils::raw_hex_to_string(&self.inner_read_buf[..bytes_read])))?;
+			}
+		}
+		else { return Err(Error::Custom("Tello: Requested socket not found in this Tello!")) }
 	}
 }
 
-impl Drone
+impl TelloDrone
 {
 	#[allow(dead_code)]
-	pub(crate) fn init(registry: Arc<Mutex<Poll>>, map: Arc<Mutex<HashMap<Token, Connection>>>) -> Result<Arc<Mutex<Self>>, Error> {
-		let command_sock = {
+	pub(crate) fn new(registry: Arc<Mutex<Poll>>, map: Arc<Mutex<HashMap<Token, Connection>>>, logger : Logger) -> Result<Arc<Mutex<Self>>, Error> {
+		let mut command_sock = {
 			const COMMAND_PORT: u16 = 8889;
 			const CONN_ADDR: Ipv4Addr = Ipv4Addr::new(192, 168, 10, 1);
 			const CONN_SOCK: SocketAddrV4 = SocketAddrV4::new(CONN_ADDR, COMMAND_PORT);
 
 			let command_sock = UdpSocket::bind(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0)))?;
-			command_sock.connect(SocketAddrV4::new(CONN_ADDR, COMMAND_PORT))?;
-
-			dbg!(registry, map);
+			command_sock.connect(SocketAddr::V4(SocketAddrV4::new(CONN_ADDR, COMMAND_PORT)))?;
 
 			command_sock
 		};
 
-		let info_sock = {
+		let mut info_sock = {
 			const INFO_PORT: u16 = 8890;
 			const CONN_ADDR: Ipv4Addr = Ipv4Addr::new(192, 168, 10, 1);
 			const CONN_SOCK: SocketAddrV4 = SocketAddrV4::new(CONN_ADDR, INFO_PORT);
 			let info_sock = UdpSocket::bind(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0)))?;
-			info_sock.connect(CONN_SOCK)?;
+			info_sock.connect(SocketAddr::V4(CONN_SOCK))?;
 
 			info_sock
 		};
 
-		let video_sock = {
+		let mut video_sock = {
 			const VIDEO_PORT: u16 = 11111;
 			const ARBITRARY_PORT: u16 = 11112;
 			const CONN_ADDR: Ipv4Addr = Ipv4Addr::new(192, 168, 10, 1);
 			const CONN_SOCK: SocketAddrV4 = SocketAddrV4::new(CONN_ADDR, VIDEO_PORT);
 
 			let video_sock = UdpSocket::bind(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0)))?;
-			video_sock.connect(CONN_SOCK)?;
+			video_sock.connect(SocketAddr::V4(CONN_SOCK))?;
 
 			video_sock
 		};
 
+		let com_token = Token(command_sock.local_addr()?.port() as usize);
+		let vid_token = Token(video_sock.local_addr()?.port() as usize);
+		let nfo_token = Token(info_sock.local_addr()?.port() as usize);
+
+		logger.info_from_string(format!("Command socket connected to: {}",	com_token.0))?;
+		logger.info_from_string(format!("Video socket connected to: {}",		vid_token.0))?;
+		logger.info_from_string(format!("Info socket connected to: {}",		nfo_token.0))?;
+
+		// Register all the sockets...
+		{
+			let poll_lock = registry.lock()?;
+			let registry = poll_lock.registry();
+
+			registry.register(&mut command_sock,	com_token, Interest::READABLE)?;
+			registry.register(&mut video_sock,		vid_token, Interest::READABLE)?;
+			registry.register(&mut info_sock,		nfo_token, Interest::READABLE)?;
+		}
+
 		let seq_number = 0;
 		let response_buffer = vec![0; 255];
 
-		command_sock.send(b"command")?;
-		command_sock.send(b"streamon")?;
-
 		sleep(Duration::from_secs(1));
 
-		Ok(Arc::new(Mutex::new(Self { command_sock, video_sock, info_sock, seq_number, response_buffer,
-		forward_percent: 0, sideway_percent: 0, rotate_percent: 0, updown_percent: 0})))
+		let this_drone = Arc::new(Mutex::new(Self {
+			command_sock,
+			video_sock,
+			info_sock,
+			seq_number,
+			response_buffer,
+			secret			: 0x1234_u16.to_be(), // Everything is little endian with the Tello, but this looks nice over the network.
+			forward_percent	: 0,
+			sideway_percent	: 0,
+			rotate_percent	: 0,
+			updown_percent	: 0,
+
+			inner_read_buf	: [0;4096],
+			logger,
+		}
+		));
+
+		{
+			let mut map_lock = map.lock()?;
+			map_lock.insert(com_token, Connection::Drone(this_drone.clone()));
+			map_lock.insert(vid_token, Connection::Drone(this_drone.clone()));
+			map_lock.insert(nfo_token, Connection::Drone(this_drone.clone()));
+		}
+
+		this_drone.lock()?.connect()?;
+
+		Ok(this_drone)
+	}
+
+	fn connect(&mut self) -> Result<(), Error>
+	{
+		let secret_bytes = self.secret.as_bytes();
+		let conn_string = format!("conn_req:{}{}", secret_bytes[0] as char, secret_bytes[1] as char);
+		self.command_sock.send(conn_string.as_bytes())?;
+
+		Ok(())
 	}
 }
--- src/drone_interface/tello/packet.rs
+++ src/drone_interface/tello/packet.rs
@@ -1,5 +1,7 @@
 use concat_arrays::concat_arrays;
-use crate::drone_interface::crc::{crc16, crc8};
+use lebe::Endian;
+use zerocopy::IntoBytes;
+use crate::drone_interface::crc::{crc16, crc16_ref, crc8, crc8_ref};
 use crate::drone_interface::tello::packet::Command::{Land, SetSticks, TakeOff};
 use crate::drone_interface::tello::packet::PacketType::{Data2, SetInfo};
 use crate::UdpSocket;
@@ -22,13 +24,14 @@ enum PacketType
 	Unknown2	= 0x07,
 }
 
+#[repr(u16)]
 enum Command
 {
 	Undefined,
-	SetSticks = 0x0050,
-	TakeOff = 0x0054,
-	Land = 0x0055,
-	Flip = 0x005c
+	SetSticks = 0x5000_u16.to_be(),
+	TakeOff = 0x5400_u16.to_be(),
+	Land = 0x5500_u16.to_be(),
+	Flip = 0x5c00_u16.to_be(),
 }
 
 #[allow(dead_code)]
@@ -41,15 +44,15 @@ struct Packet
 /// The first result is the header, the second is the crc at the end.
 fn packet_header(payload : &[u8], packet_type: PacketType, command: Command, seq : u16) -> ([u8;9], [u8;2])
 {
-	let mut header : [u8;9] = [0xCC, 00, 00, 00, 00, 00, 00, 00, 00, ];
-	let payload_size : [u8;2] = (payload.len() as u16).to_le_bytes();
-	let packet_type_info : u8 = 0b0100_0000 | (packet_type as u8).unbounded_shl(3);
-	let message_id : [u8;2] = (command as u16).to_le_bytes();
-	let crc = crc8(&header[..3]);
+	let mut header: [u8; 9] = [0xCC, 00, 00, 00, 00, 00, 00, 00, 00, ];
+	let payload_size: [u8; 2] = packet_size(payload.len() as u16);
+	let packet_type_info: u8 = 0b0100_0000 | (packet_type as u8).unbounded_shl(3);
+	let message_id: [u8; 2] = (command as u16).to_le_bytes();
 
-	let sequence_number : [u8;2] = seq.to_le_bytes();
+	let sequence_number: [u8; 2] = seq.to_le_bytes();
 	header[1] = payload_size[0];
 	header[2] = payload_size[1];
+	let crc = crc8_ref(&header[..3]);
 	header[3] = crc;
 	header[4] = packet_type_info;
 	header[5] = message_id[0];
@@ -57,10 +60,10 @@ fn packet_header(payload : &[u8], packet_type: PacketType, command: Command, seq
 	header[7] = sequence_number[0];
 	header[8] = sequence_number[1];
 
-	(header, crc16(payload).to_le_bytes())
+	let last_crc: [u8; 2] = crc16_ref(header.iter().chain(payload.iter())).to_le_bytes();
 
+	(header, last_crc)
 }
-
 /*fn create_full_packet<const N : usize>(payload : [u8;N], sequence_number : u16) -> [u8;N + 11]
 {
 	todo!()
@@ -73,9 +76,44 @@ fn packet_header(payload : &[u8], packet_type: PacketType, command: Command, seq
 
 pub fn takeoff(sequence_number : u16) -> [u8;11]
 {
+	// I want to make it this simple.
 	let (header, footer) = packet_header(&[], SetInfo, TakeOff, sequence_number);
-
 	concat_arrays!(header, footer)
+
+
+	// this implementation works right now.
+	/*let size = packet_size::<0>();
+	let seq : [u8;2] = sequence_number.to_le_bytes();
+
+
+	let mut array = [
+		0xCC,				// Constant
+		size[0], size[1],	// Size of whole packet
+		0,					// CRC-8
+		0x68,				// Packet type
+		(TakeOff as u16).as_bytes()[0], (TakeOff as u16).as_bytes()[1],			// Message ID
+		seq[0], seq[1],		// Sequence
+		0, 0,				// CRC-16
+	];
+
+	let header_and_count : [u8;3] = array[..3].try_into().unwrap();
+	let first_crc = crc8(header_and_count);
+	array[3] = first_crc;
+
+	let array_clone : [u8;9] = array[..9].try_into().unwrap();
+	let last_crc : [u8;2] = crc16(array_clone).to_le_bytes();
+	array[9] = last_crc[0];
+	array[10] = last_crc[1];
+
+	array*/
+}
+
+#[test]
+fn test_takeoff()
+{
+	let seq = u16::from_big_endian_into_current(0x5003);
+	let result = takeoff(seq);
+	assert_eq!(result, [0xcc, 0x58, 0x00, 0x7c, 0x68, 0x54, 0x00, 0x50, 0x03, 0xde, 0x68]);
 }
 
 // Each value must be between 0-100.
@@ -99,13 +137,37 @@ pub fn set_sticks(sequence_number : u16, mut rot : i16, mut ud : i16, mut lr : i
 	concat_arrays!(header, payload, footer)
 }
 
+/// This also does the endian conversion
+pub const fn packet_size(payload_len : u16) -> [u8;2]
+{
+	((payload_len + 11) << 3).to_le_bytes()
+}
+
+
+
 pub fn land(sequence_number : u16) -> [u8;12]
 {
-	const PAYLOAD : [u8;1] = [0];
+	const PAYLOAD : [u8;1] = [0x00];
 	let (header, footer) = packet_header(&PAYLOAD, SetInfo, Land, sequence_number);
+
+	/*[
+		0xCC,		// Constant
+		0x58, 0x00,	// Size of whole packet - the first byte, little endian, after its shifted 3 to the right.
+		0x7c, 0x68,
+		0x54, 0x00
+	]*/
 
 	concat_arrays!(header, PAYLOAD, footer)
 }
+
+#[test]
+fn test_land()
+{
+	let seq = 0x2a06_u16.from_big_endian_into_current();
+	let result = land(seq);
+	assert_eq!(result, [0xcc_u8, 0x60, 0x00, 0x27, 0x68, 0x55, 0x00, 0x2a, 0x06, 0x00, 0xef, 0xca]);
+}
+
 
 pub fn cancel_land(sequence_number : u16) -> [u8;12]
 {
@@ -114,5 +176,4 @@ pub fn cancel_land(sequence_number : u16) -> [u8;12]
 
 		concat_arrays!(header, PAYLOAD, footer)
 }
-
 
