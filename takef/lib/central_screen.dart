ade42e9698cbb9fe8a2be4a64e41ff7b12f65b9a
Rashid Byrdsell <RHByrdsell@outlook.com>
2026-02-17 12:10:26 -0500

    Commenting out the disconnect message for now

--- takef/lib/central_screen.dart
+++ takef/lib/central_screen.dart
@@ -67,11 +67,11 @@ class _FlightScreenState extends State<FlightScreen>{
       DeviceOrientation.landscapeRight,
     ]);
     startConnection();
-    Future.delayed(const Duration(seconds: 5), () {
+    /*Future.delayed(const Duration(seconds: 5), () {
       if (mounted) {
         disconnect(context);
       }
-    });
+    });*/
   }
 
   void startConnection() async{
