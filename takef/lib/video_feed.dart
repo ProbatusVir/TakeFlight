53932ad9ba5768dfd592f4da98e16b53e5a0eb7e
Rashid Byrdsell <RHByrdsell@outlook.com>
2026-02-17 13:29:50 -0500

    Attempt 1 at no video feed handeling

--- takef/lib/video_feed.dart
+++ takef/lib/video_feed.dart
@@ -2,7 +2,15 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:takef/disconnect.dart';
 import 'connect.dart';
+
+enum FeedState{
+  connecting,
+  live,
+  timeout,
+  disconnected,
+}
 
 class VideoFeed extends StatefulWidget{
   const VideoFeed({super.key, required this.port});
@@ -15,10 +23,26 @@ class VideoFeed extends StatefulWidget{
 class VideoFeedState extends State<VideoFeed>{
   List<Uint8List> currentFrame = [];
   Uint8List? latestFrame;
+  FeedState _feedState = FeedState.connecting;
+  Timer? _timeOut;
 
   @override
   void initState(){
     super.initState();
+  }
+
+  void resetTimeOut(){
+    //Clear timeout timer
+    _timeOut?.cancel();
+    //Reset it at 3 seconds
+    _timeOut = Timer(const Duration(seconds: 3), (){
+      if(mounted){
+        setState(() {
+          //set the state to be in timeout
+          _feedState = FeedState.timeout;
+        });
+      }
+    });
   }
 
   void onImageReceived(Uint8List frames) async {
@@ -26,7 +50,11 @@ class VideoFeedState extends State<VideoFeed>{
       print('VideoFeedState received frame of ${frames.length} bytes');
       latestFrame = frames;
       currentFrame.add(latestFrame!);
+      if(_feedState != FeedState.live && latestFrame != null){
+        _feedState = FeedState.live;
+      }
     });
+    resetTimeOut();
     /*final received = await vid.getDroneImg();
     if (received != null) {
       latestFrame = received;
@@ -38,13 +66,45 @@ class VideoFeedState extends State<VideoFeed>{
   @override
   Widget build(BuildContext context) {
     //will place a loading screen if false otherwise will show video feed
-    return latestFrame == null ? CircularProgressIndicator()
-      : Image.memory(
-      latestFrame!,
-      gaplessPlayback: true,
-      fit: BoxFit.cover,
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
+    return Stack(
+      children: [
+        if(_feedState == FeedState.live && latestFrame != null)
+          Image.memory(
+            latestFrame!,
+            gaplessPlayback: true,
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
+        if(_feedState == FeedState.connecting)
+          const CircularProgressIndicator(),
+        if(_feedState == FeedState.timeout)
+          noFeedIndicator(),
+        ///TODO::Place disconnection logic here for flight screen
+        /*if(_feedState == FeedState.disconnected)
+          disconnect(context),*/
+      ],
     );
   }
+}
+
+Widget noFeedIndicator(){
+  return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.7),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.videocam_off, color: Colors.white),
+          SizedBox(height: 8),
+          Text(
+            "No Video Feed",
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      )
+  );
 }
