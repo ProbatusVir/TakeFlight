aaf156615d98056b5384fab5511d1565ad0f4b69
Rashid Byrdsell <RHByrdsell@outlook.com>
2026-02-18 13:54:36 -0500

    debugging

--- takef/lib/flight_control_mode.dart
+++ takef/lib/flight_control_mode.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 enum ControlMode {
@@ -7,13 +8,14 @@ enum ControlMode {
 
 Future<void> saveControlMode(ControlMode mode) async{
   final prefs= await SharedPreferences.getInstance();
+  //debugPrint("Saving mode = ${mode.name}");
   await prefs.setString('control_mode', mode.name);
 }
 
 Future<ControlMode> loadControlMode() async{
   final prefs = await SharedPreferences.getInstance();
   final value = prefs.getString('control_mode');
-
+  //debugPrint("Loaded control_mode = $value");
   if(value == null) return ControlMode.joystick;
 
   return ControlMode.values.firstWhere(
